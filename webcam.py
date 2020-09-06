f7abd043a90e9b507ade3ff7673bc92ac2639391
Arpit Kushwaha <59803976+kushwahaarpit@users.noreply.github.com>
2020-09-06 22:37:03 +0530

    Add files via upload

---
+++ webcam.py
@@ -0,0 +1,13 @@
+import cv2
+
+imgcapture = cv2.VideoCapture(0)
+result = True
+
+while(result):
+    ret, frame = imgcapture.read()
+    cv2.imwrite("Test.jpg", frame)
+    result = False
+    print("Image Captured...")
+
+imgcapture.release()
+    
